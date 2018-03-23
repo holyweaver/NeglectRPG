583bd3d32ef2d58e36facb34d864bbbaec6a553e
Unknown <wlsghtpqms@naver.com>
2018-03-23 17:13:31 +0900

    김진호

---
+++ Assets/Scripts/CharMove.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharMove : MonoBehaviour {
+
+	public enum charmotion
+	{
+		idle,
+		automove,
+		clickmove,
+
+	}
+
+	public Vector2 maxpos;
+	public Vector2 minpos;
+
+	public Vector3 charpos;
+
+	public float movespeed;
+	public Vector2 movedir;
+
+	public Transform chartrans;
+
+	public int randirnumx;
+	public int randirnumy;
+
+	public charmotion icharmotion;
+
+	public int movecount = 0;
+
+
+
+	void charidle()
+	{
+
+		icharmotion = charmotion.automove;
+		
+	}
+
+	void charautomove()
+	{
+		AutoMovex ();
+		AutoMovey ();
+	}
+
+	void AutoMovex()
+	{
+		charpos.x += movespeed * randirnumx * movedir.x; //dir값 -1 0 1 / movespeed값 0.01 ~ 0.02
+
+		chartrans.position = charpos;
+	}
+		
+	void AutoMovey()
+	{
+		charpos.y += movespeed * randirnumy * movedir.y;
+
+		chartrans.position = charpos;
+	}
+
+	void ClickMove()
+	{
+
+	}
+
+	void Awake()
+	{
+		//icharmotion = charmotion.idle;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		chartrans = transform;
+		charpos = chartrans.position;
+		movedir = Vector2.one;
+		randirnumx = Random.Range (-1, 2);
+		randirnumy = Random.Range (-1, 2);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (icharmotion == charmotion.idle) 
+		{
+			
+		}
+		else if (icharmotion == charmotion.automove) 
+		{
+			charautomove ();
+		}
+
+		if (charpos.x > maxpos.x) 
+		{
+			charpos.x = maxpos.x;
+		}
+
+		if (charpos.x < minpos.x) 
+		{
+			charpos.x = minpos.x;
+		}
+
+		if (charpos.y > maxpos.y) 
+		{
+			charpos.y = maxpos.y;
+		}
+
+		if (charpos.y < minpos.y) 
+		{
+			charpos.y = minpos.y;
+		}
+		
+		
+	}
+}
---
+++ Assets/Scripts/SSSS.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Reflection;
+
+public class SSSS : MonoBehaviour {
+
+	public enum SSTate
+	{
+		Idle,
+		Attack,
+	}
+
+	public SSTate State;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (IdleState());
+	}
+	
+	IEnumerator IdleState()
+	{
+
+        yield return new WaitForSeconds(3);
+
+
+		while(true)
+		{
+			Debug.Log ("IdleState");
+			if(Input.GetKey(KeyCode.A))
+			{
+				Debug.Log ("NextState : Attack");
+				State = SSTate.Attack;
+				break;
+			}
+			yield return null;
+		}
+
+		NextState ();
+
+	}
+
+	IEnumerator AttackState()
+	{
+		while(true)
+		{
+			Debug.Log ("AttackState");
+
+			if(Input.GetKey(KeyCode.A))
+			{
+				Debug.Log ("NextState : Idle");
+				State = SSTate.Idle;
+				break;
+			}
+			yield return null;
+		}
+
+		NextState ();
+	}
+
+	void NextState()
+	{
+		string MethodName = State.ToString () + "State";
+		MethodInfo info = GetType ().GetMethod (MethodName, BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.CreateInstance);
+		StartCoroutine ((IEnumerator)info.Invoke(this, null));
+	}
+
+}
